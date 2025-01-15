a91fffee6f99eeacd856d856a013ed08d833e960
charl <charl@10.232.145.89>
2025-01-15 15:35:40 -0600

    hello?!

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message { 
 
 	public static void main(String args[]) {
-		System.out.println("  theasdl;fk   l;k j workshop! ");
+		System.out.println(" theasdl;fk   l;k j workshop! ");
 	}   
 }
